d3c26681187f4c17fb5f7dda00552957a74ab2c3
James <jcalingo@katalystos.com>
2022-05-24 17:19:21 -0400

    Updating style to flexbox

--- assets/js/calculateTip.js
+++ assets/js/calculateTip.js
@@ -1,57 +1,62 @@
-let $ = require('jquery')
-
-function clear () {
+function clear() {
   $('#total').val('')
   $('#people').val('')
 }
 
+$('#info').on("click", function() {
+  Swal.fire({
+  text:  "All you need to do is input your total (pre-tax) and the percentage you want to tip. Additionally, if you want to split the tip between a group of people, you can input that too - if you don't, the calculator will assume only one person is reponsible for the tip.",
+  confirmButtonText: "Got it!"
+  })
+})
+
 function calculateTip(total, percent, people) {
 
-var tip = total * percent
-var $tip = tip.toFixed(2)
+  let tip = total * percent
+  let $tip = tip.toFixed(2)
 
-var tipDivide = tip / people
-var $tipDivide = tipDivide.toFixed(2)
+  let tipDivide = tip / people
+  let $tipDivide = tipDivide.toFixed(2)
 
-if (isNaN(total)) {
-  Swal.fire({
-    title: 'Interesting...',
-    text: 'Your total is...wait, what is your total exactly?',
-    type: 'error',
-    confirmButtonText: 'Please put a number in for your total'
-  })
-  return false
-}
+  if (isNaN(total)) {
+    Swal.fire({
+      title: 'Interesting...',
+      text: 'Your total is...wait, what is your total exactly?',
+      type: 'error',
+      confirmButtonText: 'Please put a number in for your total'
+    })
+    return
+  }
 
-if (people < 1 || total < 1) {
-  Swal.fire({
-    title: 'HEY!',
-    text: 'Are you seriously going to skip out on tipping?',
-    type: 'error',
-    confirmButtonText: "Either way, I'm not letting you do that!"
-  })
-  return false
-}
+  if (people < 1 || total < 1) {
+    Swal.fire({
+      title: "Cannot calculate tip",
+      text: "I can't calculate a tip based on these values. Please check your vaules and try again",
+      type: 'error',
+     
+    })
+    return
+  }
 
-if (people == 1) {
-  $('#calculatedTip').html(`You should tip <strong>$${$tip}</strong>!`)
-  clear()
-  return $tip
-}
+  if (people == 1) {
+    $('#calculatedTip').html(`You should tip <strong>$${$tip}</strong>!`)
+    clear()
+    return $tip
+  }
 
-if (people > 1) {
-  $('#calculatedTip').html(`Your total tip is <strong>$${$tip}</strong>. Divided amongst the ${people} of you, each person should chip in <strong>$${$tipDivide}</strong>.<br>
+  if (people > 1) {
+    $('#calculatedTip').html(`Your total tip is <strong>$${$tip}</strong>. Divided amongst the ${people} of you, each person should chip in <strong>$${$tipDivide}</strong>.<br>
   (This is assuming you all split the bill evenly)`)
-  clear()
-  return $tip
-}
+    clear()
+    return $tip
+  }
 }
 
 $('#calculate').on('click', function (event) {
   event.preventDefault()
   let $total = $('#total').val().trim()
-let $percent = $('#tip').val().trim() / 100
-let $people = $('#people').val().trim()
+  let $percent = $('#tip').val().trim() / 100
+  let $people = $('#people').val() ? $('#people').val().trim() : 1
   calculateTip($total, $percent, $people)
-  
+
 })
